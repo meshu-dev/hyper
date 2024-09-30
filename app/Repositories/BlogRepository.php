e17e4196839b3e420ec52b2a9fda23de670a4408
Mesh <harmeshuppal@gmail.com>
2024-09-30 03:02:31 +0100

    Fix error with get blog by notion page id ftn

--- app/Repositories/BlogRepository.php
+++ app/Repositories/BlogRepository.php
@@ -4,7 +4,7 @@ namespace App\Repositories;
 
 use App\Models\Blog;
 use App\Models\NotionBlog;
-use Illuminate\Database\Query\Builder;
+use Illuminate\Database\Eloquent\Builder;
 
 class BlogRepository
 {
