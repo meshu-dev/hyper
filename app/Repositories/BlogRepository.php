47789c38f07e443227c2524c43d3530ef97402fe
Mesh <harmeshuppal@gmail.com>
2024-09-30 03:00:23 +0100

    Fix get by notion page id in Blog repository

--- app/Repositories/BlogRepository.php
+++ app/Repositories/BlogRepository.php
@@ -3,11 +3,19 @@
 namespace App\Repositories;
 
 use App\Models\Blog;
+use App\Models\NotionBlog;
+use Illuminate\Database\Query\Builder;
 
 class BlogRepository
 {
     public function getByNotionPageId(string $pageId): Blog|null
     {
-        return Blog::where('notion_page_id', $pageId)->first();
+        return Blog::whereHasMorph(
+            'blogable',
+            [NotionBlog::class],
+            function (Builder $query) use ($pageId) {
+                $query->where('notion_page_id', $pageId);
+            }
+        )->first();
     }
 }
