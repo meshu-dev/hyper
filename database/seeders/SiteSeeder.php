b2d2adc1edaaf26d3c8f3f2c349fb10665dc6d87
Mesh <contact@devpush.io>
2025-11-02 23:28:53 +0000

    Fix error when running seeder

--- database/seeders/SiteSeeder.php
+++ database/seeders/SiteSeeder.php
@@ -4,16 +4,21 @@ namespace Database\Seeders;
 
 use App\Models\Site;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
 
 class SiteSeeder extends Seeder
 {
     public function run(): void
     {
+        Schema::disableForeignKeyConstraints();
+
         Site::truncate();
 
         Site::insert([
             ['name' => 'DevPush'],
             ['name' => 'DevNudge'],
         ]);
+
+        Schema::enableForeignKeyConstraints();
     }
 }
